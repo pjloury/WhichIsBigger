2551b3254b93dc3b92952ecc89d5c993534c3e2a
Dennis Li <dennis_li@apple.com>
2016-03-20 15:30:08 -0700

    Refactored and deprecated old backend code

--- BackendServices/wolfram_api.py
+++ BackendServices/wolfram_api.py
@@ -2,13 +2,10 @@
 # -*- coding: iso-8859-15 -*-
 
 from xml.etree import cElementTree as ElementTree
-import urllib2,urllib
-# import os
-# import pickle
+import urllib2, urllib
 import argparse
 import re
 import csv
-# import sys
 import time, datetime
 
 from parse_rest.connection import register
@@ -18,7 +15,6 @@ from dateutil.parser import parse as dateparse
 
 from api_keys import *
 
-pattern = '%m/%d/%Y'
 
 __topic_re = re.compile("""(.+?)\s+\|""")
 __topic_re1 = re.compile("""(.+?)\s+\(""")
@@ -26,20 +22,11 @@ __topic_re1 = re.compile("""(.+?)\s+\(""")
 __birthday_re = re.compile("""(\d+/\d+/\d+)""")
 __height_re = re.compile("""(.*)\s+(meters)""")
 __person_height_re = re.compile(r"""(\d+)\'\s+(\d+)""")
-
 __population_re = re.compile(r"""([0-9.]+)\s+(\w+)\s+people""")
-
 __number_re = re.compile(r"""([0-9.]+)\s+(\S+)""")
-
-# __year_re = re.compile(r"""([\d\d\d\d)""")
-
-
 __weight_re = re.compile("""(.*)\s+(lb)""")
-
 __image_re = re.compile("""src=\"//(\S+)\?""")
-
 __list_re = re.compile("""\d+\s+\|\s+([^\|]*)\s+\|""")
-
 
 QUERYSTRING_IMAGE = "http://api.wolframalpha.com/v2/query?input=%s&appid=%s&format=plaintext,html"
 QUERYSTRING = "http://api.wolframalpha.com/v2/query?input=%s&appid=%s"
@@ -49,8 +36,10 @@ QUERYSTRING = "http://api.wolframalpha.com/v2/query?input=%s&appid=%s"
 
 register(APP_ID_KEY, REST_API_KEY)
 
+
 class GameItem(Object):
     pass
+
 
 class Enum(set):
     def __getattr__(self, name):
@@ -58,40 +47,11 @@ class Enum(set):
             return name
         raise AttributeError
 
-#Category = Enum(["age", "weight", "height", "population", "image", "none"])
 
 query_string = ""
 multiple_query_string = ""
 
-pickled_objects = []
-PICKLE_FILE = './Data/objects.pkl'
-
-def fix_urls():
-    all_items = GameItem.Query.all()
-
-    for item in all_items:
-
-        if hasattr(item,'tags') and item.tags != None:
-
-            tag_cs = item.tags
-
-            tag_array = tag_cs.split(',')
-
-            item.tagArray = tag_array
-
-            item.tags = None
-
-        image_url = item.photoURL
-
-        if 'http://' not in image_url and image_url != None and image_url != "":
-
-            item.photoURL = 'http://'+image_url
-
-        if image_url == 'http://':
-            item.photoURL = None
-
-        print item
-        item.save()
+############## END WOLFRAM API ##############
 
 
 def main():
@@ -102,43 +62,19 @@ def main():
 
     parser = argparse.ArgumentParser(description='wolfram to parse backend service')
 
-    parser.add_argument('--mode', dest='mode',
-                       help='script mode')
-    parser.add_argument('--category', dest='category',
-                       help='category [age, weight, height]')
-    parser.add_argument('--object', dest='object', nargs='+',
-                       help='name of the object')
     parser.add_argument('--csv', type=file, dest='csvfile',
-                       help='csv file to parse')
-    parser.add_argument('--query', dest='query',
-                       help='optional generic query')
-    parser.add_argument('--units', dest='units',
-                       help='units for generic query')
+                        help='csv file to parse')
 
     args = parser.parse_args()
-    
+
     print args
 
-    QUERY_STRINGS = {"age":'%s birthday',"weight":'%s weight',"height":'%s height',"none":'%s',"population":'%s population'}
-    MULTIPLE_QUERY_STRINGS = {"age":'oldest %s',"weight":'heaviest %s',"height":'tallest %s',"none":'%s',"population":"none"}
+    QUERY_STRINGS = {"age": '%s birthday', "weight": '%s weight', "height": '%s height', "none": '%s',
+                     "population": '%s population'}
 
-    category = args.category
-    MODE = args.mode
-
-    OBJECT = ''
-
-    if args.object:
-        OBJECT = ' '.join(args.object)
     csvfile = args.csvfile
 
-    GENERIC_QUERY = ''
-
-    if args.query:
-        GENERIC_QUERY = args.query
-
-    UNITS = args.units
-
-    #parse a csv file for entities to query
+    # parse a csv file for entities to query
     if csvfile:
 
         print "loading csv"
@@ -151,11 +87,11 @@ def main():
                 continue
 
             OBJECT = row[0]
-            TAGS = row[1]
+            TAGS = row[1]  # all tags in quotes
             QUERY = row[2].lower()
             UNITS = row[3].lower()
 
-            #check for existing parse entry
+            # check for existing parse entry
             exists = GameItem.Query.all().filter(name=OBJECT)
             if len(exists) > 0:
                 print "Duplicate found"
@@ -165,15 +101,15 @@ def main():
 
             if "celebrity" in TAGS:
 
-                categories = ["age","height"]
+                categories = ["age", "height"]
 
             elif "athlete" in TAGS:
 
-                categories = ["age","height","weight"]
+                categories = ["age", "height", "weight"]
 
             elif "person" in TAGS:
 
-                categories = ["age","weight"]
+                categories = ["age", "weight"]
 
             elif "tall structure" in TAGS:
 
@@ -206,7 +142,7 @@ def main():
 
             for CATEGORY in categories:
 
-                #gracefully catch exception and move to next query
+                # gracefully catch exception and move to next query
                 try:
 
                     if not QUERY:
@@ -214,93 +150,14 @@ def main():
                     else:
                         query_string = "%s " + QUERY
 
-                    single_query(OBJECT,CATEGORY,UNITS,TAGS)
+                    single_query(OBJECT, CATEGORY, UNITS, TAGS)
 
                 except Exception as e:
                     print e
                     pass
 
-    #else query manually
-    elif category and MODE and OBJECT:
-
-        CATEGORY = category
-
-        # if category == "age":
-        #     CATEGORY = Category.age
-        # elif category == "weight":
-        #     CATEGORY = Category.weight
-        # elif category == "height":
-        #     CATEGORY = Category.height
-        # elif category == "population":
-        #     CATEGORY = Category.population
-        # elif category == "image":
-        #     CATEGORY = Category.image
-        # else:
-        #     CATEGORY = Category.none
-
-        #query_string = %s string
-
-        if GENERIC_QUERY:
-            query_string = "%s " + GENERIC_QUERY
-        else:
-            query_string = QUERY_STRINGS[category]
-            multiple_query_string = MULTIPLE_QUERY_STRINGS[category]
-
-        #Collection: eg. tallest buildings, tallest mountains
-        if MODE == "collection":
-            objects = get_list_of_results(OBJECT)
-
-            for obj in objects:
-                single_query(obj,CATEGORY,UNITS)
-
-        #aggregate -> get multiple attributes for the object
-        elif MODE == "aggregate":
-
-            for CATEGORY in ["age","weight","height"]:
-                query_string = QUERY_STRINGS[CATEGORY]
-                single_query(OBJECT,CATEGORY,UNITS)
-
-        else:
-
-            single_query(OBJECT,CATEGORY,UNITS)
-
     else:
         raise ValueError('Please refer to commandline arguments')
-
-"""
-get_list_of_results
-
-returns the list of entities that matches the TOPIC
-
-e.g. tallest buildings, tallest mountains
-
-currently there is no check if such a list of entities is valid/returned by the query
-
-"""
-def get_list_of_results(TOPIC):
-
-    QUERY = multiple_query_string % TOPIC
-
-    BUILT_QUERY = QUERYSTRING % (QUERY.replace(' ','%20'),APP_ID)
-
-    xml = urllib2.urlopen(BUILT_QUERY).read()
-
-    print xml
-
-    root = ElementTree.fromstring(xml)
-
-    text = root.find("./pod[@id='Result']").find('subpod').find('plaintext').text
-
-    results = []
-
-    for line in text.split('\n'):
-
-        m = __list_re.match(line)
-
-        if m:
-            results.append(m.group(1))
-
-    return results
 
 
 """
@@ -309,6 +166,7 @@ attempts to find the topic for a given XML root
 returns a string representing the topic
 
 """
+
 
 def get_topic(root):
     print root
@@ -328,16 +186,15 @@ def get_topic(root):
         raise NameError('Error parsing topic')
 
 
-def image_query(object):
+def image_query(obj):
+    obj = obj.encode('utf-8')
 
-    object = object.encode('utf-8')
+    QUERY = urllib.quote(obj)
 
-    QUERY = urllib.quote(object)
-
-    BUILT_QUERY = QUERYSTRING_IMAGE % (QUERY,APP_ID)
+    BUILT_QUERY = QUERYSTRING_IMAGE % (QUERY, APP_ID)
 
     # if os.path.exists(PICKLE_PATH):
-    #     xml = pickle.load(open(PICKLE_PATH,'rb'))
+    # xml = pickle.load(open(PICKLE_PATH,'rb'))
     #
     # else:
     #     xml = urllib2.urlopen(BUILT_QUERY).read()
@@ -365,9 +222,7 @@ def image_query(object):
         return None
 
 
-
-def parse_age(QUERY,root):
-    
+def parse_age(QUERY, root):
     value = root.find("./pod[@title='Date formats']").find('subpod').find('plaintext').text
 
     if "month/day/year" in value:
@@ -377,12 +232,12 @@ def parse_age(QUERY,root):
         if m:
             QUANTITY = m.group(1)
 
-            #convert scientifc notation to number
-            QUANTITY = int(time.mktime(time.strptime(QUANTITY, pattern)))
+            # convert scientific notation to number
+            QUANTITY = int(time.mktime(time.strptime(QUANTITY, '%m/%d/%Y')))
 
             UNIT = "date"
 
-            return (QUANTITY,UNIT)
+            return (QUANTITY, UNIT)
 
         else:
             raise NameError('No match for birthday query')
@@ -391,8 +246,7 @@ def parse_age(QUERY,root):
         raise NameError('No results found for query: %s' % QUERY)
 
 
-def parse_population(QUERY,root):
-
+def parse_population(QUERY, root):
     value = root.find("./pod[@id='Result']").find('subpod').find('plaintext').text
 
     if "people" in value:
@@ -405,14 +259,14 @@ def parse_population(QUERY,root):
             modifier = m.group(2)
 
             if modifier == "million":
-                QUANTITY = QUANTITY*float(10**6)
+                QUANTITY = QUANTITY * float(10 ** 6)
             elif modifier == "billion":
-                QUANTITY = QUANTITY*float(10**9)
+                QUANTITY = QUANTITY * float(10 ** 9)
 
-            #convert scientifc notation to number
+            # convert scientific notation to number
             UNIT = "people"
 
-            return (QUANTITY,UNIT)
+            return (QUANTITY, UNIT)
 
         else:
             raise NameError('No match for population query')
@@ -421,20 +275,18 @@ def parse_population(QUERY,root):
         raise NameError('No results found for query: %s' % QUERY)
 
 
-
-def parse_height(QUERY,root):
-
+def parse_height(QUERY, root):
     result = root.find("./pod[@id='Result']").find('subpod').find('plaintext').text
     print result
 
     m = __person_height_re.match(result)
 
     if m:
-        QUANTITY = int(m.group(1))*12 + int(m.group(2))
+        QUANTITY = int(m.group(1)) * 12 + int(m.group(2))
 
         UNIT = "inches"
 
-        return (QUANTITY,UNIT)
+        return (QUANTITY, UNIT)
 
     else:
 
@@ -451,11 +303,11 @@ def parse_height(QUERY,root):
                     if m:
                         QUANTITY = m.group(1)
 
-                        #convert scientifc notation to number
-                        QUANTITY = float(QUANTITY.replace(u"×10^","E+").replace('~',''))
+                        # convert scientific notation to number
+                        QUANTITY = float(QUANTITY.replace(u"×10^", "E+").replace('~', ''))
 
                         UNIT = m.group(2)
-                        return (QUANTITY,UNIT)
+                        return (QUANTITY, UNIT)
 
                     else:
                         raise NameError('No match for height query')
@@ -466,8 +318,7 @@ def parse_height(QUERY,root):
         raise NameError('No match for height query')
 
 
-def parse_weight(QUERY,root):
-
+def parse_weight(QUERY, root):
     result = root.find("./pod[@id='Result']").find('subpod').find('plaintext').text
 
     m = __weight_re.match(result)
@@ -475,13 +326,12 @@ def parse_weight(QUERY,root):
     if m:
         QUANTITY = m.group(1)
 
-        #convert scientifc notation to number
-        QUANTITY = float(QUANTITY.replace(u"×10^","E+").replace('~',''))
+        # convert scientific notation to number
+        QUANTITY = float(QUANTITY.replace(u"×10^", "E+").replace('~', ''))
 
         UNIT = m.group(2)
 
-        return (QUANTITY,UNIT)
-
+        return (QUANTITY, UNIT)
 
     else:
 
@@ -500,11 +350,11 @@ def parse_weight(QUERY,root):
                     if m:
                         QUANTITY = m.group(1)
 
-                        #convert scientifc notation to number
-                        QUANTITY = float(QUANTITY.replace(u"×10^","E+").replace('~',''))
+                        # convert scientific notation to number
+                        QUANTITY = float(QUANTITY.replace(u"×10^", "E+").replace('~', ''))
 
                         UNIT = m.group(2)
-                        return (QUANTITY,UNIT)
+                        return (QUANTITY, UNIT)
                     else:
                         raise NameError('No match for weight query')
 
@@ -514,15 +364,12 @@ def parse_weight(QUERY,root):
         raise NameError('No match for weight query')
 
 
-def parse(root,UNITS):
-
+def parse(root, UNITS):
     value = root.find("./pod[@id='Result']").find('subpod').find('plaintext').text
 
     print value
 
     m = __number_re.search(value)
-
-
 
     if m:
         QUANTITY = float(m.group(1))
@@ -542,7 +389,7 @@ def parse(root,UNITS):
             try:
 
                 dt = dateparse(str(int(QUANTITY)))
-                QUANTITY = (dt-datetime.datetime(1970,1,1)).total_seconds()
+                QUANTITY = (dt - datetime.datetime(1970, 1, 1)).total_seconds()
 
 
             except Exception as e:
@@ -557,29 +404,28 @@ def parse(root,UNITS):
 
     else:
 
-        #check if it is a date
+        # check if it is a date
         try:
 
             dt = dateparse(value)
 
-            QUANTITY = (dt-datetime.datetime(1970,1,1)).total_seconds()
+            QUANTITY = (dt - datetime.datetime(1970, 1, 1)).total_seconds()
 
             UNIT = "date"
 
         except:
             raise NameError('Could not parse!')
 
-    return (QUANTITY,UNIT)
+    return (QUANTITY, UNIT)
 
 
-def single_query(object,CATEGORY,UNITS="",TAGS=[]):
-
-    #ensure all non-ascii characters are converted to html entities
-    #OBJECT = OBJECT.encode("ascii", "xmlcharrefreplace")
+def single_query(object, CATEGORY, UNITS="", TAGS=[]):
+    # ensure all non-ascii characters are converted to html entities
+    # OBJECT = OBJECT.encode("ascii", "xmlcharrefreplace")
 
     QUERY = urllib.quote(query_string % object)
 
-    BUILT_QUERY = QUERYSTRING % (QUERY,APP_ID)
+    BUILT_QUERY = QUERYSTRING % (QUERY, APP_ID)
 
     xml = urllib2.urlopen(BUILT_QUERY).read()
     #print xml
@@ -588,23 +434,24 @@ def single_query(object,CATEGORY,UNITS="",TAGS=[]):
     # for child in root:
     #     print child.tag, child.attrib
 
-    #use City, Country format (exclude province)
+    # use City, Country format (exclude province)
 
     print root
-    TOPIC = get_topic(root)
-    topicItems = [x.strip() for x in TOPIC.split(',')]
-    if len(topicItems)>2:
-        TOPIC = topicItems[0] + ", " + topicItems[-1]
+    # TOPIC = get_topic(root)
+    # topicItems = [x.strip() for x in TOPIC.split(',')]
+    # if len(topicItems) > 2:
+    #     TOPIC = topicItems[0] + ", " + topicItems[-1]
+    TOPIC = object
 
     PHOTO = ''
     object = object.encode('utf-8')
     image_url = image_query(object)
     if image_url:
-        PHOTO = "http://"+image_url
+        PHOTO = "http://" + image_url
 
     elif "city" in TAGS and not PHOTO:
         country = [x.strip() for x in TOPIC.split(',')][-1]
-        countryItems  = GameItem.Query.filter(name=country).limit(1)
+        countryItems = GameItem.Query.filter(name=country).limit(1)
         for countryItem in countryItems:
             if (countryItem.photoURL):
                 PHOTO = countryItem.photoURL
@@ -614,33 +461,33 @@ def single_query(object,CATEGORY,UNITS="",TAGS=[]):
     UNIT = ''
     QUANTITY = -1
 
-    #assume we are just looking for the image
+    # assume we are just looking for the image
     if CATEGORY == "image":
 
         value = root.find("./pod[@title='Image']").find('markup').text
         m = __image_re.search(value)
         if m:
-            PHOTO = "http://"+m.group(1)
+            PHOTO = "http://" + m.group(1)
 
     elif CATEGORY == "age":
 
-        QUANTITY,UNIT = parse_age(QUERY,root)
-      
+        QUANTITY, UNIT = parse_age(QUERY, root)
+
     elif CATEGORY == "population":
 
-        QUANTITY,UNIT = parse_population(QUERY,root)
+        QUANTITY, UNIT = parse_population(QUERY, root)
 
     elif CATEGORY == "height":
 
-        QUANTITY,UNIT = parse_height(QUERY,root)
+        QUANTITY, UNIT = parse_height(QUERY, root)
 
     elif CATEGORY == "weight":
 
-        QUANTITY,UNIT = parse_weight(QUERY,root)
+        QUANTITY, UNIT = parse_weight(QUERY, root)
 
     else:
 
-        QUANTITY,UNIT = parse(root,UNITS)
+        QUANTITY, UNIT = parse(root, UNITS)
 
     ############## PARSE API ##############
 
@@ -648,5 +495,76 @@ def single_query(object,CATEGORY,UNITS="",TAGS=[]):
     print "Finished " + TOPIC
     kv.save()
 
+    ############## End PARSE API ##############
+
+
 if __name__ == "__main__":
     main()
+
+"""
+get_list_of_results (DEPRECATED)
+
+returns the list of entities that matches the TOPIC
+
+e.g. tallest buildings, tallest mountains
+
+currently there is no check if such a list of entities is valid/returned by the query
+
+"""
+
+
+def get_list_of_results(TOPIC):
+    QUERY = multiple_query_string % TOPIC
+
+    BUILT_QUERY = QUERYSTRING % (QUERY.replace(' ', '%20'), APP_ID)
+
+    xml = urllib2.urlopen(BUILT_QUERY).read()
+
+    print xml
+
+    root = ElementTree.fromstring(xml)
+
+    text = root.find("./pod[@id='Result']").find('subpod').find('plaintext').text
+
+    results = []
+
+    for line in text.split('\n'):
+
+        m = __list_re.match(line)
+
+        if m:
+            results.append(m.group(1))
+
+    return results
+
+
+"""
+fix_urls (DEPRECATED)
+Used previously to fix bad urls
+"""
+
+
+def fix_urls():
+    all_items = GameItem.Query.all()
+
+    for item in all_items:
+
+        if hasattr(item, 'tags') and item.tags != None:
+            tag_cs = item.tags
+
+            tag_array = tag_cs.split(',')
+
+            item.tagArray = tag_array
+
+            item.tags = None
+
+        image_url = item.photoURL
+
+        if 'http://' not in image_url and image_url != None and image_url != "":
+            item.photoURL = 'http://' + image_url
+
+        if image_url == 'http://':
+            item.photoURL = None
+
+        print item
+        item.save()
